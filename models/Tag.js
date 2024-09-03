eaa3242bd92e87124103efd67cd8641cc8bf65b0
otekolste <58699635+otekolste@users.noreply.github.com>
2024-09-03 19:39:18 -0400

    created Tag model

---
+++ models/Tag.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Tag extends Model {}
+
+Tag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "user",
+});
+
+module.exports = Tag;
