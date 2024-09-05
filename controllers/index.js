40d22a2e13795d486869e017fc83f65f73f29b9d
James Jackson <jjackson0228@gmail.com>
2024-09-05 19:40:38 -0400

    added post-routes.js file inside api routes in controller. Moved code for questions and answers get routes to post-routes.js. Made html routes in home-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const userRoutes = require('./user-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
-router.use('./', userRoutes);
+router.use('/', homeRoutes); // Use homeController for root '/'
 
 module.exports = router;
