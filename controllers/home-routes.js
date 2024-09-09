31c28e6f9b6d946a56498e2f3744be51b1182675
otekolste <58699635+otekolste@users.noreply.github.com>
2024-09-09 18:56:52 -0400

    updated dashboard, rendered posts on dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,31 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { Question, User, Answer, Tag } = require("../models");
 
 // html Landing page route
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Render the landing page (e.g., Handlebars view)
-  res.render('landing', {
-    title: 'Welcome to Answer-Hive!',
+  res.render("landing", {
+    title: "Welcome to Answer-Hive!",
   });
 });
 //html route for the login page with a simple title
-router.get('/login', (req, res) => {
-  res.render('login', {
-    title: 'Login to the Answer-Hive!',
+router.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login to the Answer-Hive!",
   });
 });
 
 //html route for sign up page
-router.get('/signup', (req, res) => {
-  res.render('signup', {
-    title: 'Sign up for Answer-Hive!',
+router.get("/signup", (req, res) => {
+  res.render("signup", {
+    title: "Sign up for Answer-Hive!",
   });
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard', {
-    title: 'Welcome to the Hive Bee!',
-  });
+router.get("/dashboard", async (req, res) => {
+  try {
+    console.log("hi!");
+    const questionData = await Question.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Tag,
+        },
+      ],
+      order: [["createdDate", "DESC"]],
+    });
+    const questions = questionData.map((question) =>
+      question.get({ plain: true })
+    );
+    console.log(questions);
+    res.render("dashboard", {
+      questions,
+      loggedIn: req.session.loggedIn,
+      title: "Welcome to the Hive Bee!",
+    });
+  } catch (e) {
+    res.status(500).json(e);
+    console.log(e);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-require('dotenv').config();
+require("dotenv").config();
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {},
@@ -23,17 +23,17 @@ const sess = {
 app.use(session(sess));
 
 const hbs = exphbs.create({
-  defaultLayout: 'main', // Set default layout to 'main.handlebars'
-  partialsDir: path.join(__dirname, 'views/partials'), // Optional: Set path for partials
+  defaultLayout: "main", // Set default layout to 'main.handlebars'
+  partialsDir: path.join(__dirname, "views/partials"), // Optional: Set path for partials
 });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
