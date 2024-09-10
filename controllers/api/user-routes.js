1ea318d66b49eaa1d142f5fc3a6cb84c5957ddf5
twkim6440 <bighelper6440@gmail.com>
2024-09-09 20:31:00 -0400

    POST route for Logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,16 +22,14 @@ router.post("/", async (req, res) => {
   }
 });
 
-
-
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -40,7 +38,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
@@ -48,13 +46,23 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+
+// Logout router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      // Destroy the session
+      res.status(204).end(); // End response process
+    });
+  } else {
+    res.status(404).end(); // If user isn't logged in, respond with not found and end response process
+  }
 
 
 
