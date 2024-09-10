829e70b47dd8398cd91683b11fe78b3755e68a63
twkim6440 <bighelper6440@gmail.com>
2024-09-10 16:15:59 -0400

    POST route to create new tag added.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
+const tagRoutes = require('./tag-routes');
 
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -115,4 +115,16 @@ router.get('/answer/:id', async (req, res) => {
   }
 });
 
+// POST Route to create new Tag.
+router.post('/', async (req, res) => {
+    try {
+        const newTag = await Tag.create({
+            tag_name: req.body.tag_name 
+        });
+        res.status(201).json(newTag); 
+    } catch (err) {
+        res.status(400).json({ message: 'Error creating tag', error: err.message });
+    }
+});
+
 module.exports = router;
