a4a229e5e837f0d00e128ea82d587502604e259a
James Jackson <jjackson0228@gmail.com>
2024-09-10 21:10:30 -0400

    fioxed Tag.js and fixed other errors. Cant get Post new question to work

--- models/Tag.js
+++ models/Tag.js
@@ -11,7 +11,7 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    tag_name: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
--- seeds/questionData.js
+++ seeds/questionData.js
@@ -1,13 +1,15 @@
-const { Question } = require("../models");
+const { Question } = require('../models');
 
 const questionData = [
   {
-    content: "What is the difference between let and const in JavaScript?",
+    content: 'What is the difference between let and const in JavaScript?',
+    title: 'Coding help',
     user_id: 1, // Replace with valid user ID
     createdDate: new Date(), // Current timestamp
   },
   {
-    content: "How do you create a REST API using Express?",
+    content: 'How do you create a REST API using Express?',
+    title: 'Api help',
     user_id: 2, // Replace with valid user ID
     createdDate: new Date(), // Current timestamp
   },
