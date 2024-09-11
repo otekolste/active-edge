1bf65f28fc1f684f1267b3c24b384f5c82f94e71
otekolste <58699635+otekolste@users.noreply.github.com>
2024-09-11 14:54:56 -0400

    created post-details, allow user to answer + render answers on post-details page

--- controllers/api/answer-routes.js
+++ controllers/api/answer-routes.js
@@ -1,14 +1,17 @@
-const router = require('express').Router();
-const { Answer } = require('../../models');
+const router = require("express").Router();
+const { Answer } = require("../../models");
 
 //create new answer
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const answerData = await Answer.create({
-      ...req.body,
-      user_id: req.session.user_id,
+      content: req.body.content,
+      user_id: req.body.user_id,
+      question_id: req.body.question_id,
     });
-    res.status(200).json(answerData);
+    req.session.save(() => {
+      res.status(200).json(answerData);
+    });
   } catch (err) {
     res.status(500).json(err); // Catch error
   }
